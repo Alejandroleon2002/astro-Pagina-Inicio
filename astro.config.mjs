e8f5b255af7d46364fa06da5394c23471e8f7c7f
Alejandroleon2002 <alex.leonm17.04@gmail.com>
2026-06-15 17:29:39 +0200

    Final del tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({site: "https://astropaginainicio-qpdv--4321--5162ecc8.local-corp.webcontainer.io"});
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astropaginainicio-qpdv--4321--5162ecc8.local-corp.webcontainer.io",
+  integrations: [preact()]
+});
