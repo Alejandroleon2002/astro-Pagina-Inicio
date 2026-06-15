939cd70f76d2adfb56e4cdaeee24b80ee6854780
Alejandroleon2002 <alex.leonm17.04@gmail.com>
2026-06-15 17:06:28 +0200

    prueba rss.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,5 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({site: "https://astropaginainicio-qpdv--4321--5162ecc8.local-corp.webcontainer.io"});
+
